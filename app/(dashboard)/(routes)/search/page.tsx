8a86c104057600dc29f8d3ad3a005a9ff66cf7a5
Bolaji.Akerele <bolaji.akerele@vista.co>
2025-01-16 20:36:04 +0000

    update11

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,41 +1,4 @@
-import { db } from "@/lib/db";
-import { Categories } from "./_components/categories";
-import { SearchInput } from "@/components/search-input";
-import { GetCourses } from "@/actions/get-courses";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-import { CoursesList } from "@/components/courses-list";
-
-const SearchPage = async (
-    // {
-//   searchParams,
-// }: {
-//   searchParams: { title?: string; categoryId?: string };
-// }
-) => {
-  const { userId } = await auth();
-
-  // Redirect if no user is authenticated
-  if (!userId) {
-    return redirect("/");
-  }
-
-//   // Extract `title` and `categoryId` from searchParams with defaults
-//   const { title = "", categoryId = "" } = searchParams || {};
-
-//   // Fetch categories from the database
-//   const categories = await db.category.findMany({
-//     orderBy: {
-//       name: "asc",
-//     },
-//   });
-
-//   // Fetch courses based on the searchParams and userId
-//   const courses = await GetCourses({
-//     userId,
-//     title,
-//     categoryId,
-//   });
+const SearchPage = () => {
 
   return (
     <>
